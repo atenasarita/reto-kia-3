abc466bf730167c83619b05da74d269324cb08f9
DiegoVa18 <111297904+DiegoVa18@users.noreply.github.com>
2025-06-11 02:35:52 -0600

    Update dashboard.routes.js

--- backend/routes/dashboard.routes.js
+++ backend/routes/dashboard.routes.js
@@ -3,7 +3,10 @@ const router = express.Router();
 const auth = require('../middlewares/auth');
 const dashboardController = require('../controllers/dashboard.controller');
 
-// Aquí solo /dashboard, porque en index.js tienes /api
-router.get('/dashboard', auth, dashboardController.getDashboard);
+router.get('/dashboard', auth, dashboardController.getDashboard); 
+
+router.get('/residuos-por-area', auth, dashboardController.getResiduosPorArea);
+
+router.get('/residuos-por-mes', auth, dashboardController.getResiduosPorMes);
 
 module.exports = router;
